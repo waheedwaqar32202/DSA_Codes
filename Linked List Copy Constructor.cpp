03cf4717a1d48eba3b656621a8ea959d0f479a59
waheedwaqar32202 <80830376+waheedwaqar32202@users.noreply.github.com>
2021-04-05 02:37:04 +0500

    Add files via upload

---
+++ Linked List Copy Constructor.cpp
@@ -0,0 +1,203 @@
+#include<iostream>
+using namespace std;
+
+class LinkedList;
+class Node
+{
+	friend class LinkedList;
+	int data;
+	Node* next;
+
+};
+
+class LinkedList
+{
+	Node* head;
+	Node* tail;
+
+public:
+	LinkedList()
+	{
+		head = nullptr;
+		tail = nullptr;
+	}
+	void insert(int value)
+	{
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = nullptr;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+	}
+
+	void display()
+	{
+		if (head == nullptr)
+		{
+			cout << "List is empty" << endl;
+			return;
+		}
+		Node* temp = head;
+
+		while (temp != nullptr)
+		{
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+	}
+	void deleteNode(int value)
+	{
+		if (head == nullptr)
+		{
+			cout << "List is Empty" << endl;
+			return;
+		}
+
+		Node* curr = head;
+		Node* prev = nullptr;
+
+		while (curr != nullptr && curr->data != value)
+		{
+			prev = curr;
+			curr = curr->next;
+		}
+		if (curr == nullptr)
+		{
+			cout << "Value not found" << endl;
+			return;
+		}
+		else
+		{
+			if (curr == head && curr == tail)  //if(head==tail)
+			{
+				head = nullptr;
+				tail = nullptr;
+			}
+			else if (curr == head)
+			{
+				head = head->next;
+				delete curr;
+				curr = nullptr;
+			}
+			else if (curr == tail)
+			{
+				tail = prev;
+				tail->next = nullptr;
+				delete curr;
+				curr = nullptr;
+			}
+			else
+			{
+				prev->next = curr->next;
+				delete curr;
+				curr = nullptr;
+			}
+		}
+
+
+
+
+	}
+
+	bool search(int value)
+	{
+		if (head == nullptr)
+		{
+			return false;
+		}
+
+		Node* curr = head;
+		while (curr != nullptr && curr->data != value)
+		{
+
+			curr = curr->next;
+		}
+
+		if (curr == nullptr)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	LinkedList(const LinkedList & list)
+	{
+		if (list.head == nullptr)
+		{
+			head = tail = nullptr;
+			return;
+		}
+
+		Node* temp = list.head;
+		Node* newNode = nullptr;
+
+		while (temp != nullptr)
+		{
+			newNode = new Node();
+			newNode->data = temp->data;
+			newNode->next = nullptr;
+
+			if (head == nullptr)
+			{
+				head = newNode;
+				tail = newNode;
+			}
+			else
+			{
+				tail->next = newNode;
+				tail = newNode;
+			}
+			temp = temp->next;
+		}
+	} 
+	~LinkedList()
+	{
+		Node* curr = head;
+		Node* prev = nullptr;
+
+		while (curr != nullptr)
+		{
+			prev = curr;
+			curr = curr->next;
+			delete prev;
+		}
+		head = nullptr;
+		tail = nullptr;
+	}
+
+
+};
+
+
+int main()
+{
+	LinkedList list1;
+
+
+	list1.insert(2);
+	list1.insert(3);
+	list1.insert(5);
+
+	list1.display();
+	cout << endl;
+	LinkedList list2 = list1;   // LinkedList list2(list1);
+
+	list1.~LinkedList();
+	list1.display();
+
+	list2.display();
+	cout << endl;
+
+	return 0;
+}
